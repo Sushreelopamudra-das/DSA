292482d8065cf61f7c17ff7320a24a46b0827f8c
Sushree Lopamudra Das <137261645+Sushreelopamudra-das@users.noreply.github.com>
2024-03-11 10:31:00 +0530

    Add files via upload

--- UnionArray.java
+++ UnionArray.java
@@ -1,42 +1,49 @@
+import java.util.ArrayList;
 
-public class UnionArray{
-  static void union(int a[],int b[],int m,int n){
-    int i=0, j=0;
-  while(i<m && j<n){
-      while(i+1<m && a[i]==a[i+1])
-      i++;
-        while(j+1<n && a[j]==a[j+i])
-        j++;
-      
-      if(a[i]<a[j])
-      System.out.print(a[i++]);
-      
-      else if(b[j]<a[i]){
-        System.out.println(b[j++]);
-      }
-      else{
-        System.out.println(a[j]);
-        i++;
-      }
+public class UnionArray {
+
+    static ArrayList<Integer> FindUnion(int arr1[], int arr2[], int n, int m) {
+        int i = 0, j = 0; // pointers
+       ArrayList<Integer > Union=new ArrayList<>(); // Uninon vector
+       while (i < n && j < m) {
+         if (arr1[i] <= arr2[j]) // Case 1 and 2
+         {
+           if (Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
+             Union.add(arr1[i]);
+           i++;
+         } else // case 3
+         {
+           if (Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
+             Union.add(arr2[j]);
+           j++;
+         }
+       }
+       while (i < n) // IF any element left in arr1
+       {
+         if (Union.get(Union.size()-1) != arr1[i])
+           Union.add(arr1[i]);
+         i++;
+       }
+       while (j < m) // If any elements left in arr2
+       {
+         if (Union.get(Union.size()-1) != arr2[j])
+           Union.add(arr2[j]);
+         j++;
+       }
+       return Union;
     }
-  
+     public static void main(String args[]) {
+       int n = 10, m = 7;
+       int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+       int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+       ArrayList<Integer> Union = FindUnion(arr1, arr2, n, m);
+       System.out.println("Union of arr1 and arr2 is ");
+       for (int val: Union)
+         System.out.print(val+" ");
+     }
     
-    while(i<m){
-      while(i+1<m && a[i]==a[i+1])
-      System.out.println(a[i++]);
-    }
-      while(j<n){
-        while(j+1<n && b[j]==b[j+1])
-        System.out.println(b[j++]);
-      }
-    }
-  
-  public static void main(String []args){
-    int[] a={1,3,4,5,7,9};
-    int[] b={2,3,4,5,8,9};
-    int m=a.length;
-    int n=b.length;
-    union(a,b,m,n);
-  }
 }
-  
+
+
+
+
