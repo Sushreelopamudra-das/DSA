0fa5873845167766b894383596481ff275bae85f
Sushree Lopamudra Das <137261645+Sushreelopamudra-das@users.noreply.github.com>
2024-03-17 11:45:52 +0530

    Add files via upload

---
+++ LongestrsubArraywithgivenSumk.java
@@ -0,0 +1,30 @@
+public class LongestrsubArraywithgivenSumk {
+  public static int longestSubarray(int []a,long k){
+    int n = a.length;
+
+    int left = 0, right = 0;
+    long sum = a[0];
+    int maxLen = 0;
+
+    while(right < n){
+
+        while(left <= right && sum > k){
+            sum -= a[left];
+            left++;
+        }
+        if(sum == k){
+            maxLen = Math.max(maxLen, right-left+1);
+        }
+        right++;
+        if(right<n) sum += a[right];
+    }
+    return maxLen;
+  }
+
+    public static void main(String[] args) {
+        int[] a ={2,3,5,1,9};
+        long k = 10;
+        int len = longestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: "+ len);
+    }
+}
