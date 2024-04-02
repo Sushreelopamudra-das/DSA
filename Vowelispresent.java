01044184fbaffd0499b0e914695da88678ad156c
Sushree Lopamudra Das <137261645+Sushreelopamudra-das@users.noreply.github.com>
2024-04-02 18:01:56 +0530

    Add files via upload

---
+++ Vowelispresent.java
@@ -0,0 +1,13 @@
+public class Vowelispresent {
+        public static void main(String[] args) {
+            System.out.println(stringContainsVowels("Hello")); // true
+            System.out.println(stringContainsVowels("TV")); // false
+        }
+    
+        public static boolean stringContainsVowels(String input) {
+            return input.toLowerCase().matches(".*[aeiou].*");
+        }
+    
+    
+    
+}
