ad089968df8b08e8ffa80d4e6e15ae8950b89f6a
Sushree Lopamudra Das <137261645+Sushreelopamudra-das@users.noreply.github.com>
2024-04-04 21:44:43 +0530

    Add files via upload

---
+++ Swapnumbers.java
@@ -0,0 +1,15 @@
+public class Swapnumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+    
+        System.out.println("a is " + a + " and b is " + b);
+    
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    
+        System.out.println("After swapping, a is " + a + " and b is " + b);
+        }
+    
+}
