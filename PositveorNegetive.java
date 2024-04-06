bed988c7462ef7bb684dd1dcbf59e6464792f3eb
Sushree Lopamudra Das <137261645+Sushreelopamudra-das@users.noreply.github.com>
2024-04-06 21:34:34 +0530

    Add files via upload

---
+++ PositveorNegetive.java
@@ -0,0 +1,19 @@
+package interview;
+
+public class PositveorNegetive {
+
+    public static void main(String[] args) {
+        int num = 5;
+        
+        // Condition to check if the number is negative or positive
+        if (num >= 0) {
+            if (num == 0)
+                System.out.println("Zero");
+            else
+                System.out.println("The number is positive");
+        } else
+            System.out.println("The number is negative");
+    }
+}
+
+
