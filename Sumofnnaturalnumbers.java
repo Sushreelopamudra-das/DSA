acb169502949162e4cadb3b6feee41678d6b550f
Sushree Lopamudra Das <137261645+Sushreelopamudra-das@users.noreply.github.com>
2024-05-11 21:22:08 +0530

    Add files via upload

---
+++ Sumofnnaturalnumbers.java
@@ -0,0 +1,16 @@
+package interview;
+
+public class Sumofnnaturalnumbers {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum =0;
+        for(int i=1;i<=n;i++){
+            sum = sum + i;
+            System.out.println(sum);
+
+
+        }
+    }
+    
+}
+
