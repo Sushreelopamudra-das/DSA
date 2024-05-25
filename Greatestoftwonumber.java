88c736f7659747a1141a626447ff051579d74d49
Sushree Lopamudra Das <137261645+Sushreelopamudra-das@users.noreply.github.com>
2024-05-25 18:39:31 +0530

    Add files via upload

---
+++ Greatestoftwonumber.java
@@ -0,0 +1,19 @@
+package interview;
+
+public class Greatestoftwonumber {
+    public static void main(String[] args) {
+        
+       int num1 = 12;
+       int num2 = 11;
+       
+       if(num1 == num2)
+       System.out.println("Both are equal");
+       else if(num1 > num2)
+       System.out.println(num1+ "is greater");
+       else
+        System.out.println(num2+ "is greater");
+       
+
+    }
+    
+}
