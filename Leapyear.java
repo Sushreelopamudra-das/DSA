a7b1064e17e515e6d8d046b2ae91fe0939ca1f15
Sushree Lopamudra Das <137261645+Sushreelopamudra-das@users.noreply.github.com>
2024-05-31 22:08:43 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class Leapyear {
+    public static void main(String[] args) {
+        
+        int year = 2023;
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 !=0))
+         System.out.println(year + " is a leap year");
+
+        else
+         System.out.println(year + "is not a leap year");
+    }
+    
+}
