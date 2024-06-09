82c914668f81eec069cc4a67e7913788c489ffd3
Sushree Lopamudra Das <137261645+Sushreelopamudra-das@users.noreply.github.com>
2024-06-09 12:13:06 +0530

    Add files via upload

---
+++ LastOccurrence.java
@@ -0,0 +1,29 @@
+package Binarysearch;
+
+public class LastOccurrence {
+    public static int solve(int n, int key, int[] v) {
+        int start = 0;
+        int end = n - 1;
+        int res = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (v[mid] == key) {
+                res = mid;
+                start = mid + 1; // Move right to find the last occurrence
+            } else if (key < v[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        int key = 13;
+        int[] v = {3, 4, 13, 13, 13, 20, 40};
+        System.out.println(solve(n, key, v)); // Output should be 4
+    }
+}
